6494086ab85d41aaf22107e4a848fa0965d0fff5
Tuxist <tuxist@googlemail.com>
2017-04-16 12:51:58 +0000

    add pi working parser

--- src/modules/camera.cpp
+++ src/modules/camera.cpp
@@ -20,39 +20,76 @@ along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 ************************************************************************/
 
 #include <cstdio>
+#include <iostream>
 #include <sstream>
+#include <sys/wait.h>
+
 #include <httppp/config.h>
 #include <httppp/connections.h>
 #include <httppp/http.h>
 #include "../modules.h"
 
 namespace pidoorkeepder {
+  class RecordCamera {
+  public:
+    RecordCamera(libhttppp::Connection *curcon){
+      pid_t pid;
+      char buf[512];
+      int fd[2], len = 0;
+
+      if(pipe(fd) < 0){
+        std::cerr << "Cannot create pipe\n";
+        return;
+      }
+
+      pid = fork();
+      if(pid < 0){ /* failure */
+        std::cerr << "forkfailed";
+        return;
+      }
+      if(pid > 0) { /* parent */
+        close(fd[1]);
+        while((len = read(fd[0], &buf, 512)) > 0) {
+          curcon->addSendQueue((const char*)&buf,512);
+        }
+      } else { /* child */
+        close(fd[0]);
+        dup2(fd[1], 1);
+        dup2(fd[1], 2);
+        close(fd[1]);
+        if(execle("raspivid","-t 10 -o -",NULL,NULL) < 0)
+          perror("exec");
+        quick_exit(EXIT_SUCCESS);
+      }
+      int status;
+      waitpid(-1, &status, 0);
+    };
+  };
+
   class Camera : public pidoorkeepder::ModuleAPI {
   public:
     virtual const char *getName(){
       return "CameraController";
     }
-    
+
     virtual const char *getControllerPath(){
       return "/pidoorkeepder/camera.mp4";
     }
-    
+
     virtual void runModul(libhttppp::Connection *curcon){
-      grabpicam(curcon);
-    }
-    
-    virtual ~Camera(){
-        
-    }
-  private:
-    void grabpicam(libhttppp::Connection *curcon){
+      std::stringstream idxstream;
       libhttppp::HttpResponse curres;
       curres.setState(HTTP200);
       curres.setVersion(HTTPVERSION(1.1));
       curres.setContentType("video/mp4");
       curres.send(curcon,NULL,0);
-//       curcon->addSendQueue(); 
+      RecordCamera recordcam(curcon);
     }
+
+    virtual ~Camera(){
+
+    }
+
 };
 
 // the class factories
